2e0c66f3e8c525f1f9205c483bb3e640a2bc639a
user1 <user>
2022-11-14 21:04:25 +0300

    change main

--- TP2/TP2.cpp
+++ TP2/TP2.cpp
@@ -32,6 +32,55 @@ int return_int()
 	return atof(S);
 }
 
+int** qsortRecursive(int** mas, int size) {	//сортировка с использованием массива
+	//Указатели в начало и в конец массива
+	int i = 0;
+	int j = size - 1;
+	int tmp[3];
+	//Центральный элемент массива
+	int mid = mas[size / 2][0];
+
+	//Делим массив
+	do {
+		//Пробегаем элементы, ищем те, которые нужно перекинуть в другую часть
+		//В левой части массива пропускаем(оставляем на месте) элементы, которые меньше центрального
+		while (mas[i][0] < mid) {
+			i++;
+		}
+		//В правой части пропускаем элементы, которые больше центрального
+		while (mas[j][0] > mid) {
+			j--;
+		}
+
+		//Меняем элементы местами
+		if (i <= j) {
+			tmp[0] = mas[i][0];
+			tmp[1] = mas[i][1];
+			tmp[2] = mas[i][2];
+			mas[i][0] = mas[j][0];
+			mas[i][1] = mas[j][1];
+			mas[i][2] = mas[j][2];
+			mas[j][0] = tmp[0];
+			mas[j][1] = tmp[1];
+			mas[j][2] = tmp[2];
+
+			i++;
+			j--;
+		}
+	} while (i <= j);
+
+
+	//Рекурсивные вызовы, если осталось, что сортировать
+	if (j > 0) {
+		//"Левый кусок"
+		qsortRecursive(mas, j + 1);
+	}
+	if (i < size) {
+		//"Првый кусок"
+		qsortRecursive(&mas[i], size - i);
+	}
+	return mas;
+}
 
 
 int main() {
@@ -42,16 +91,10 @@ int main() {
 	Fabric* fabric1_input = new Fabric();
 	Fabric* fabric2_input = new Fabric();
 
-	//Queue_3 Keeper; // create queue. хранит все объекты класса фабрика
-
-	/*Queue* copyKeeper;
-	Queue* copyKeeper_1;*/
 	Queue* Keeper2 = new Queue();
-	Queue* copyKeeper1 = new Queue();
-	Queue* Keeper_temp = new Queue();
+
 	int** mass_order2;
-	//int* mass_order;	//элементы Keeper
-	//int value='s';
+
 	setlocale(LC_ALL, "Russian");
 	system("cls");
 	int n = 1;
@@ -59,29 +102,25 @@ int main() {
 	int a = NULL;
 	int b = NULL;
 	int c = NULL;
-	int d = NULL;
-	int nomer_stroki_read = 0;
-	string text1 = "null";
-	string text2 = "null";
-	string text3 = "null";
-	string text4_read = "null";
-	int size_car = 0;
-	int size_worker = 0;
-	int size_furniture = 0;
+	int d = NULL;	//номер строки
+
 	int size_order = 0;
-	int size_order_temp = 0;
-	int order_pay_max = -1;//максимальное число плательщика
-	int order_pay_min = -1;//минимальное число плательщика
-	int steps = 0;
-	int i = 0;
-	int i2 = 0;
+	int i3 = 0;	//счетчик массива строки где одинаковое количество символов пунктуации
+	int* mas_strok_cout = new int[1];
+	//чтение файла
+	string text_temp = "null";
+	string* text_max_punkt = new string[1];
+	string* text_max_punkt2 = new string[1];
+	int nomer_stroki_read = 0;
+	//string text1 = "null";
+
 	cout << "1:добавить order	2:вывод order	3:сортировка order	4:вывод информации о плательщике	5:чтение файла	6:выход";
 
 	while (n != 6) {
 		a = NULL;
 		b = NULL;
 		c = NULL;
-		d = NULL;
+
 		cout << "\nномер операции = ";
 		n = return_int();
 		switch (n) {
@@ -90,11 +129,7 @@ int main() {
 			cout << "\nдобавить order";
 			cout << "\nрасчетный счет плательщика = ";
 			a = return_int();
-			if (order_pay_max == -1 && order_pay_min == -1)
-			{
-				order_pay_max = a;
-				order_pay_min = a;
-			}
+
 			cout << "\nрасчетный счет получателя = ";
 			b = return_int();
 			cout << "\nперечисляемая сумма в рублях = ";
@@ -103,7 +138,7 @@ int main() {
 
 			Keeper2->insert(*fabric1_input);
 			fabric1_input = new Fabric();
-			size_worker++;
+
 			break;
 		case 2:
 			// print queue
@@ -112,9 +147,9 @@ int main() {
 
 			break;
 		case 3://сортировка
-			//int* mass_order = new int[Keeper2->Queue_Size()];
 			size_order = Keeper2->Queue_Size();
 			mass_order2 = new int* [size_order];
+
 			for (int count = 0; count < size_order; count++)
 				mass_order2[count] = new int[2];
 
@@ -145,126 +180,111 @@ int main() {
 			break;
 
 
-		case 5:
+		case 5:	//сделал номер строки где много символов пунктуации. сделать вывод самой строки в результате
+			a = 0;
+			b = 0;//кол-во символов пунктуации прошлой
+			c = 0;//кол-во символов пунктуации сейчас
+			d = 0;//номер строки
+
+
 			cout << "\nчтения файла result.txt\n";
 			in_txt.open("result.txt"); // окрываем файл для чтения
+
 			if (in_txt.is_open())
 			{
-				while (getline(in_txt, text4_read))
+
+				while (getline(in_txt, text_temp))
 				{
-					nomer_stroki_read = 0;	//для номера строки
-					cout << text4_read << endl;	//вывод всего текста
-					if (text4_read == "worker_info")	//данные работника. отформатированные. проделать для машины и мебели
+
+					//cout << text4_read << endl;	//вывод всего текста
+					for (a = 0; a < text_temp.size(); a++)
 					{
-						while (nomer_stroki_read != 5)
+						cout << text_temp[a];
+
+						if (text_temp[a] == ',' || text_temp[a] == ';' || text_temp[a] == ':' || text_temp[a] == '-' || text_temp[a] == '(' ||
+							text_temp[a] == ')' || text_temp[a] == '"')
+							b++;
+						if (text_temp[a] == '.' || text_temp[a] == '!' || text_temp[a] == '?')
 						{
-							getline(in_txt, text4_read);
-							switch (nomer_stroki_read)
+
+
+							if (c < b)//запоминаю КОЛ-ВО ПУНКТУАЦИИ
 							{
-							case 0:
-								text1 = text4_read.substr(7, text4_read.size() - 1);
-								break;
-							case 1:
-								text2 = text4_read.substr(7, text4_read.size() - 1);
-								break;
-							case 2:
-								a = stoi(text4_read.substr(6, text4_read.size() - 1));
-								break;
-							case 3:
-								text3 = text4_read.substr(10, text4_read.size() - 1);
-								break;
-							case 4:
-								b = stoi(text4_read.substr(12, text4_read.size() - 1));
-								break;
-							}
-
-							cout << text4_read << endl;	//вывод всего текста
-							nomer_stroki_read++;
-						}
-
-						fabric1_input->Fab_worker_add(text1, text2, a, text3, b);
-						Keeper2->insert(*fabric1_input);
-						fabric1_input = new Fabric();
-					}
-
-					if (text4_read == "car_info")	//данные машины
-					{
-
-						while (nomer_stroki_read != 3)
-						{
-							getline(in_txt, text4_read);
-							switch (nomer_stroki_read)
-							{
-							case 0:
-								text1 = text4_read.substr(9, text4_read.size() - 1);
-								break;
-							case 1:
-								text2 = text4_read.substr(9, text4_read.size() - 1);
-								break;
-							case 2:
-								text3 = text4_read.substr(14, text4_read.size() - 1);
-								break;
+								c = b;
 
 							}
-
-							cout << text4_read << endl;	//вывод всего текста
-							nomer_stroki_read++;
+							b = 0;
 						}
 
-						fabric1_input->Fab_car_add(text1, text2, text3);
-						Keeper2->insert(*fabric1_input);
-						fabric1_input = new Fabric();
 					}
 
-					if (text4_read == "furniture_info")	//данные мебели
-					{
-						while (nomer_stroki_read != 6)
-						{
-							getline(in_txt, text4_read);
-							switch (nomer_stroki_read)
-							{
-							case 0:
-								text1 = text4_read.substr(8, text4_read.size() - 1);
-								break;
-							case 1:
-								a = stoi(text4_read.substr(10, text4_read.size() - 1));
-								break;
-							case 2:
-								b = stoi(text4_read.substr(11, text4_read.size() - 1));
-								break;
-							case 3:
-								c = stoi(text4_read.substr(11, text4_read.size() - 1));
-								break;
-							case 4:
-								text2 = text4_read.substr(9, text4_read.size() - 1);
-								break;
-							case 5:
-								text3 = text4_read.substr(12, text4_read.size() - 1);
-								break;
-							case 6:
-								d = stoi(text4_read.substr(9, text4_read.size() - 1));
-								break;
-
-
-							}
-
-							cout << text4_read << endl;	//вывод всего текста
-							nomer_stroki_read++;
-						}
-
-						fabric1_input->Fab_furniture_add(text1, a, b, c, text2, text3, d);
-						Keeper2->insert(*fabric1_input);
-						fabric1_input = new Fabric();
-					}
-
-
-
-
+					cout << "\n";
+					//text1 = text4_read.substr(7, text4_read.size() - 1);
+					//cout << text4_read << endl;	//вывод всего текста
+					nomer_stroki_read++;
 				}
 			}
-			else
-				cout << "файл не найден";
+
+			cout << "\n кол-во запятых = " << c;
+			//c--;
 			in_txt.close();
+			a = 0;
+			b = 0;//кол-во символов пунктуации прошлой
+			//c = 0;//кол-во символов пунктуации сейчас
+			d = 0;//номер строки
+
+			in_txt.open("result.txt"); // окрываем файл для чтения
+
+			if (in_txt.is_open())
+			{
+
+				while (getline(in_txt, text_temp))
+				{
+					for (a = 0; a < text_temp.size(); a++)
+					{
+						//cout << text_temp[a];
+
+						if (text_temp[a] == ',' || text_temp[a] == ';' || text_temp[a] == ':' || text_temp[a] == '-' || text_temp[a] == '(' ||
+							text_temp[a] == ')' || text_temp[a] == '"')
+							b++;
+						if (text_temp[a] == '.' || text_temp[a] == '!' || text_temp[a] == '?')
+						{
+
+							//d_temp++;
+							//запоминает первую максимальную строку. если кол-во символов в следующией столько же, то не запоминает
+							if (c == b)
+							{
+								i3++;
+								text_max_punkt2 = new string[i3];
+								for (d = 0; d < i3 - 1; d++)
+									text_max_punkt2[d] = text_max_punkt[d];
+
+								text_max_punkt = new string[i3];
+								for (d = 0; d < i3 - 1; d++)
+									text_max_punkt[d] = text_max_punkt2[d];
+								text_max_punkt[i3 - 1] = text_temp;
+							}
+
+
+							b = 0;
+						}
+					}
+				}
+			}
+
+			cout << "\nкол-во строк = " << nomer_stroki_read;
+			cout << "\nстроки с наибольшим количеством символов пунктуации: \n";
+			for (a = 0; a < i3; a++)
+				cout << "\n nomber = " << a << " == " << text_max_punkt[a];
+			in_txt.close();
+			a = 0;
+			b = 0;//кол-во символов пунктуации прошлой
+			c = 0;//кол-во символов пунктуации сейчас
+			d = 0;//номер строки
+			nomer_stroki_read = 0;
+			text_max_punkt = new string[1];
+			text_max_punkt2 = new string[1];
+			i3 = 0;
 			break;
 		case 8:
 			//close program
